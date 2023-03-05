290a0ff8efb73774b1368fb3587b951000171534
unknown <erenbaltali16@gmail.com>
2023-03-05 16:14:03 +0300

    katıl fix

--- komutlar/katıl.js
+++ komutlar/katıl.js
@@ -33,9 +33,23 @@ module.exports = {
             connection.subscribe(player);
             player.play(resource);
         });
+
         connection.on(VoiceConnectionStatus.Disconnected, (oldState, newState)=>{
            console.log(oldState);
         });
+
+        voiceConnection.on('stateChange', (oldState, newState) => {
+            const oldNetworking = Reflect.get(oldState, 'networking');
+            const newNetworking = Reflect.get(newState, 'networking');
+          
+            const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
+              const newUdp = Reflect.get(newNetworkState, 'udp');
+              clearInterval(newUdp?.keepAliveInterval);
+            }
+          
+            oldNetworking?.off('stateChange', networkStateChangeHandler);
+            newNetworking?.on('stateChange', networkStateChangeHandler);
+          });
 
     }
 }
