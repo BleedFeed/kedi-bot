54e408cba4b4f4a95934675746bde6d7d1f84bb2
BleedFeed <65342830+BleedFeed@users.noreply.github.com>
2023-03-07 23:09:41 +0000

    eylence

--- index.js
+++ index.js
@@ -61,6 +61,9 @@ const server = http.createServer((req,res)=>{
 	if(req.url === '/radyo'){
 		console.log('radyoya dinleyici geldi');
 		res.writeHead(200,{'Content-Type':'audio/mpeg','Connection':'keep-alive'});
+		res.socket.on('end',()=>{
+			writableStreams.splice(writableStreams.indexOf(res),1);
+		});
 		res.socket.setKeepAlive(true);
 		writableStreams.push(res);
 	}
