fd2b52880ae775edf485f3430057f3ede59b75d3
bleedfeed <erenbaltali16@gmail.com>
2023-03-09 20:55:11 +0300

    annen

--- komutlar/radyo.js
+++ komutlar/radyo.js
@@ -67,7 +67,7 @@ function getAudioStream(url){
         '-b:a','128k',
         'pipe:4'],{stdio:['ignore','ignore','ignore','pipe','pipe']});
         ytdlStream.pipe(ffmpegProcess.stdio[3]);
-        resolve(ffmpegProcess.stdio[4].pipe(new Throttle(16384)));
+        resolve(ffmpegProcess.stdio[4].pipe(new Throttle(8192)));
     });
 }
 
