6c8c8a473db826964c2d331cd33687bbe77e493d
bleedfeed <erenbaltali16@gmail.com>
2023-03-10 22:01:26 +0300

    asda

--- komutlar/geç.js
+++ komutlar/geç.js
@@ -2,10 +2,10 @@ const {SlashCommandBuilder} = require('discord.js');
 
 module.exports = {
     data:new SlashCommandBuilder()
-	.setName('katıl')
-	.setDescription('odanıza katılıp radyoyu çalar'),
+	.setName('geç')
+	.setDescription('geçer'),
     async execute(interaction,writableStreams){
         require('../utils/readable').emit('end');
-        
+
     }
 }
