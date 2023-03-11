b59dd4bc5796aa2a8fa6fa67b43725c38b95edb9
bleedfeed <erenbaltali16@gmail.com>
2023-03-12 01:06:49 +0300

    ytdlp

--- utils/songs.js
+++ utils/songs.js
@@ -1,1 +1,1 @@
-module.exports = ['https://www.youtube.com/watch?v=U0TXIXTzJEY','https://www.youtube.com/watch?v=zd8IFDgQCUc'];
+module.exports = ['https://www.youtube.com/watch?v=k1yYG0vMgQ8','https://www.youtube.com/watch?v=zd8IFDgQCUc'];
